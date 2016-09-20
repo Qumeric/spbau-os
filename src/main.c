aedb819e95c7e0bfc5086c92f8a29952b9448d44
niksaz <nikitasazanovich@gmail.com>
2016-09-20 13:51:47 -0400

    bug

--- src/main.c
+++ src/main.c
@@ -13,19 +13,32 @@ static void clearbit(uint8_t *data, int bit)
     *data &= ~(1 << bit);
 }
 
+static void serial_port_print(char *mes) 
+{
+    int i = 0;
+    while (mes[i] != '\0')
+    {
+        uint8_t p5 = in8(serial_port + 5);
+        if (p5 & (1 << 5)) 
+        {
+            out8(serial_port + 0, mes[i++]);
+        }
+    }
+}
+
 void main(void)
 {
-    // disable interrupts 
-    uint8_t p3 = in8(serial_port + 3);
-    setbit(&p3, 7);
-    out8(serial_port + 3, p3);
-    out8(serial_port + 1, 0);
-
     // div
-    p3 = in8(serial_port + 3);
-    clearbit(&p3, 7); 
+    uint8_t p3 = in8(serial_port + 3);
+    setbit(&p3, 7); 
     out8(serial_port + 3, p3);
     out8(serial_port + 0, 1);
+    out8(serial_port + 1, 0);
+
+    // disable interrupts 
+    p3 = in8(serial_port + 3);
+    clearbit(&p3, 7);
+    out8(serial_port + 3, p3);
     out8(serial_port + 1, 0);
 
     // frame format
@@ -37,14 +50,6 @@ void main(void)
     clearbit(&p3, 7);
     out8(serial_port + 3, p3);
 
-    char mes[] = "Hello, world!\n\0";
-    int i = 0;
-    while (mes[i] != '\0')
-    {
-        uint8_t p5 = in8(serial_port + 5);
-        if (p5 & (1 << 5)) 
-        {
-            out8(serial_port + 0, mes[i++]);
-        }
-    }
+    char *mes = "Hello, world!\n\0";
+    serial_port_print(mes);  
 }
